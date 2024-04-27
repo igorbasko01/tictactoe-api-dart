a6660cc3c1321a98e70d33a0805200c0d8c64196
igorbasko01 <igorbasko@gmail.com>
2024-04-27 12:23:03 +0300

    base commit

---
+++ bin/tic_tac_toe_api.dart
@@ -0,0 +1,5 @@
+import 'package:tic_tac_toe_api/tic_tac_toe_api.dart' as tic_tac_toe_api;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tic_tac_toe_api.calculate()}!');
+}
---
+++ test/tic_tac_toe_api_test.dart
@@ -0,0 +1,8 @@
+import 'package:tic_tac_toe_api/tic_tac_toe_api.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
